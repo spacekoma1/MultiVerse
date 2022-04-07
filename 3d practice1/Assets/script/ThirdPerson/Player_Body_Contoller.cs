d03d05da22e7da951fa30022f24020a444e242d0
spacekoma1 <dldnwn8@gmail.com>
2022-04-08 01:42:59 +0900

    Version0.1
    player 캐릭터 추가
    player 캐릭터 애니메이션 추가
    player 캐릭터 애니메이터 추가
    Player_Body_Controller 코드에 애니메이션 관련 코드 추가

---
+++ 3d practice1/Assets/script/ThirdPerson/Player_Body_Contoller.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Body_Contoller : MonoBehaviour
+{
+    [SerializeField] private Transform Player_Follower;
+
+    public float Player_WalkSpeed = 3.0f;
+    public float Player_RunSpeed = 7.0f;
+
+    private Animator player_animator;
+    private CharacterController player_controller;
+    private Vector3 MoveDir;
+
+    void Awake() {
+        player_animator = GetComponent<Animator>();
+    }
+
+    void Start() {
+        player_controller = GetComponent<CharacterController>();
+    }
+
+    void Update() {
+        //인풋처리
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+
+        MoveDir = new Vector3(inputX, 0, inputZ);
+        MoveDir = transform.TransformDirection(MoveDir); //로컬에서 월드로 변환
+        if (Input.GetButton("Left Shift"))  {//뛰는지 걷는지 판단해서 각 스피드 곱하기
+            MoveDir *= Player_RunSpeed;
+            player_animator.SetBool("IsRun", true); //Run 스테이트로 넘어가기(애니메이션)
+        }
+        else {
+            MoveDir *= Player_WalkSpeed;
+            player_animator.SetBool("IsRun", false); //Run 스테이트 끝
+        }
+
+        //움직이는 키 입력시
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical")) {
+            //카메라 방향을 보기
+            transform.localRotation = Player_Follower.localRotation;
+            //Walk 스테이트로 넘어가기 (애니메이션)
+            player_animator.SetBool("IsWalk", true);
+            player_animator.SetFloat("WalkDir_Ver", inputZ);
+        }
+        else {
+            player_animator.SetBool("IsWalk", false);
+        }
+
+        
+    }
+
+    void FixedUpdate()
+    {
+        player_controller.Move(MoveDir * Time.fixedDeltaTime);
+    }
+}
---
+++ 3d practice1/Assets/script/ThirdPerson/Player_Cam_Rotater.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Player_Cam_Rotater
+{
+    public float XSensitivity = 2f;
+    public float YSensitivity = 2f;
+    public bool clampVerticalRotation = true;
+    public float MinimumX = -80F;
+    public float MaximumX = 80F;
+    public bool smooth = false;
+    public float smoothTime = 5f;
+    public bool lockCursor = true;
+
+
+    private Quaternion m_SubCoordinateTargetRot;
+    private Quaternion m_CameraTargetRot;
+    private bool m_cursorIsLocked = true;
+
+    public void Init(Transform subcoordinate, Transform camera) {
+        m_SubCoordinateTargetRot = subcoordinate.localRotation;
+        m_CameraTargetRot = camera.localRotation;
+    }
+
+
+    public void LookRotation(Transform subcoordinate, Transform camera) {
+        float yRot = Input.GetAxis("Mouse X") * XSensitivity;
+        float xRot = Input.GetAxis("Mouse Y") * YSensitivity;
+
+        m_SubCoordinateTargetRot *= Quaternion.Euler(0f, yRot, 0f);
+        m_CameraTargetRot *= Quaternion.Euler(-xRot, 0f, 0f);
+
+        if (clampVerticalRotation)
+            m_CameraTargetRot = ClampRotationAroundXAxis(m_CameraTargetRot);
+
+        if (smooth) {
+            subcoordinate.localRotation = Quaternion.Slerp(subcoordinate.localRotation, m_SubCoordinateTargetRot,
+                smoothTime * Time.deltaTime);
+            camera.localRotation = Quaternion.Slerp(camera.localRotation, m_CameraTargetRot,
+                smoothTime * Time.deltaTime);
+        }
+        else {
+            subcoordinate.localRotation = m_SubCoordinateTargetRot;
+            camera.localRotation = m_CameraTargetRot;
+        }
+
+        UpdateCursorLock();
+    }
+
+    public void SetCursorLock(bool value) {
+        lockCursor = value;
+        if (!lockCursor)
+        {//we force unlock the cursor if the user disable the cursor locking helper
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void UpdateCursorLock() {
+        //if the user set "lockCursor" we check & properly lock the cursos
+        if (lockCursor)
+            InternalLockUpdate();
+    }
+
+    private void InternalLockUpdate()  {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            m_cursorIsLocked = false;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            m_cursorIsLocked = true;
+        }
+
+        if (m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (!m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    Quaternion ClampRotationAroundXAxis(Quaternion q) {
+        //어차피 x축회전만 하므로 x값은 sin(theta/2)
+        //q.x / q.w = tan(theta/2)
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x / q.w);
+
+        //float angleX = Mathf.Acos(q.w) * 2.0f * Mathf.Rad2Deg; // -값 이 안나오고 절댓값으로 나옴 -> 폐기
+
+        angleX = Mathf.Clamp(angleX, MinimumX, MaximumX);
+
+        //x축 회전만 하므로 y = z = 0;
+        q = Quaternion.Euler(angleX, 0f, 0f);
+
+        return q;
+    }
+}
