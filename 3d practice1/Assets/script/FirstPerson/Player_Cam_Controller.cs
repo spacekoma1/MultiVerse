02f8a2e5cd505a8a25145a3163c1a792dd507657
spacekoma1 <dldnwn8@gmail.com>
2022-04-07 17:57:35 +0900

    MultiVerse_Unity_Ver0.0
    프로젝트 토대가 될 유니티 프로젝트 첫 버전입니다.

---
+++ 3d practice1/Assets/script/FirstPerson/Player_Cam_Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Cam_Controller : MonoBehaviour
+{
+    [SerializeField] private Transform Player_Body;
+    [SerializeField] private Camera Player_Cam;
+    [SerializeField] private Transform Player_Follower;
+    [SerializeField] private Player_Cam_Rotater m_Player_Cam_Rotater;
+
+    void Start() {
+        m_Player_Cam_Rotater.Init(Player_Follower, Player_Cam.transform);
+    }
+
+    void Update() {
+        //3인칭 카메라 회전
+        m_Player_Cam_Rotater.LookRotation(Player_Follower, Player_Cam.transform);
+        //카메라가 플레이어 바디 따라다님
+        Player_Follower.position = Player_Body.position + (Vector3.up * 2);
+        
+    }
+}
---
+++ 3d practice1/Assets/script/FirstPerson/Player_Cam_Rotater.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Player_Cam_Rotater
+{
+    public float XSensitivity = 2f;
+    public float YSensitivity = 2f;
+    public bool clampVerticalRotation = true;
+    public float MinimumX = -80F;
+    public float MaximumX = 80F;
+    public bool smooth = false;
+    public float smoothTime = 5f;
+    public bool lockCursor = true;
+
+
+    private Quaternion m_SubCoordinateTargetRot;
+    private Quaternion m_CameraTargetRot;
+    private bool m_cursorIsLocked = true;
+
+    public void Init(Transform subcoordinate, Transform camera) {
+        m_SubCoordinateTargetRot = subcoordinate.localRotation;
+        m_CameraTargetRot = camera.localRotation;
+    }
+
+
+    public void LookRotation(Transform subcoordinate, Transform camera) {
+        float yRot = Input.GetAxis("Mouse X") * XSensitivity;
+        float xRot = Input.GetAxis("Mouse Y") * YSensitivity;
+
+        m_SubCoordinateTargetRot *= Quaternion.Euler(0f, yRot, 0f);
+        m_CameraTargetRot *= Quaternion.Euler(-xRot, 0f, 0f);
+
+        if (clampVerticalRotation)
+            m_CameraTargetRot = ClampRotationAroundXAxis(m_CameraTargetRot);
+
+        if (smooth) {
+            subcoordinate.localRotation = Quaternion.Slerp(subcoordinate.localRotation, m_SubCoordinateTargetRot,
+                smoothTime * Time.deltaTime);
+            camera.localRotation = Quaternion.Slerp(camera.localRotation, m_CameraTargetRot,
+                smoothTime * Time.deltaTime);
+        }
+        else {
+            subcoordinate.localRotation = m_SubCoordinateTargetRot;
+            camera.localRotation = m_CameraTargetRot;
+        }
+
+        UpdateCursorLock();
+    }
+
+    public void SetCursorLock(bool value) {
+        lockCursor = value;
+        if (!lockCursor)
+        {//we force unlock the cursor if the user disable the cursor locking helper
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    public void UpdateCursorLock() {
+        //if the user set "lockCursor" we check & properly lock the cursos
+        if (lockCursor)
+            InternalLockUpdate();
+    }
+
+    private void InternalLockUpdate()  {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            m_cursorIsLocked = false;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            m_cursorIsLocked = true;
+        }
+
+        if (m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (!m_cursorIsLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    Quaternion ClampRotationAroundXAxis(Quaternion q) {
+        //어차피 x축회전만 하므로 w를 통해서 몇도 회전했는지만 뽑아내기
+        float angleX = Mathf.Acos(q.w) * 2.0f * Mathf.Rad2Deg;
+
+        angleX = Mathf.Clamp(angleX, MinimumX, MaximumX);
+
+        q = Quaternion.Euler(angleX, 0f, 0f);
+
+        return q;
+    }
+}
